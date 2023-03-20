9ca994c3a9997d5aadc1abc0dc5cb0e583e5320b
Duy Anh <93511014+DuyanhAtros92@users.noreply.github.com>
2023-03-20 23:34:58 +0700

    assignment 5

---
+++ 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+void coinToss();
+int inputValidate(int);
+
+int main()
+{
+    int choice;
+    unsigned seed = time(0);
+    srand(seed);
+
+    cout << "How many times should the coin be tossed? ";
+    choice = inputValidate(choice);
+
+    for (int i = 0; i < choice; i++)
+        coinToss();
+
+    return 0;
+}
+
+int inputValidate(int num)
+{
+    while(!(cin >> num) || num < 0)
+    {
+        cout << "Error. An integer above 0 must be entered: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return num;
+}
+
+void coinToss()
+{
+    const int MIN_VALUE = 1,
+              MAX_VALUE = 2;
+    
+    int coin;
+
+    coin = (rand() % (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
+
+    cout << (coin == MIN_VALUE ? "Heads" : "Tails") << endl;
+}
